4a3d52ab8745fc8bcdffd3560fbefd26d3e197a9
Patrick Annan <you@example.compatrickannan@hotmail.com>
2024-02-19 20:17:32 -0500

    added adoption pics and carousel pics and also added the description under the adoption pictures

--- frontend/src/Components/Storefront.js
+++ frontend/src/Components/Storefront.js
@@ -1,60 +1,60 @@
-import React from 'react'
+
+import React from 'react';
 import NavigationBar from './Navbar';
 import Footer from './Footer';
+import { Container, Carousel, Row, Col, Button } from 'react-bootstrap';
 
-const storeFront = () => {
+const pictureData = [
+  { id: 1, src: './Carousel_pics./Playing_1', description: 'Two friends playing' },
+  { id: 2, src: './Carousel_pics./Playing_2', description: 'Playing in the backyard' },
+  { id: 2, src: './Carousel_pics./Playing_3', description: 'Cat and Dog frolicking' },
+  { id: 2, src: './Carousel_pics./Playing_4', description: 'Curious pack of dogs in the park' },
+  { id: 2, src: './Carousel_pics./Playing_5', description: 'The Family Pet' },
+  // Add more picture data as needed
+];
+
+function App() {
   return (
-<div>
-  <NavigationBar />
-  <Container style={{ marginTop: '30px' }}>
+    <div>
+      <NavigationBar />
+      <Container style={{ marginTop: '30px' }}>
+        {/* First Carousel */}
         <Carousel>
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src="img1.jpg"
-              alt="First slide"
+              src={pictureData[0].src}
+              alt={`Slide ${pictureData[0].id}`}
               height="80"
             />
           </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="img2.jpg"
-              alt="Second slide"
-              height="80"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="img3.jpg"
-              alt="Second slide"
-              height="80"
-            />
-          </Carousel.Item>
-          {/* Add more Carousel.Item for additional images */}
+          {/* Add more Carousel.Item for additional images in the first series */}
         </Carousel>
+
+        {/* Two rows of five pictures with short descriptions */}
         <Row className="mt-3">
-          {[1, 2, 3, 4, 5].map((imageNumber) => (
-            <Col key={imageNumber} md={2} className="mb-2">
+          {pictureData.slice(0, 5).map((picture) => (
+            <Col key={picture.id} md={2} className="mb-2">
               <img
-                src={`your_image_url_${imageNumber}.jpg`}
-                alt={`Image ${imageNumber}`}
+                src={picture.src}
+                alt={`Image ${picture.id}`}
                 className="img-fluid"
                 height="80"
               />
+              <p className="mt-2">{picture.description}</p>
             </Col>
           ))}
         </Row>
         <Row className="mb-3">
-          {[6, 7, 8, 9, 10].map((imageNumber) => (
-            <Col key={imageNumber} md={2} className="mb-2">
+          {pictureData.slice(5, 10).map((picture) => (
+            <Col key={picture.id} md={2} className="mb-2">
               <img
-                src={`your_image_url_${imageNumber}.jpg`}
-                alt={`Image ${imageNumber}`}
+                src={picture.src}
+                alt={`Image ${picture.id}`}
                 className="img-fluid"
                 height="80"
               />
+              <p className="mt-2">{picture.description}</p>
             </Col>
           ))}
         </Row>
