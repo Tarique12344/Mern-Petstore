0cf32328f865cc5e961cbd204675d3b64f793ad4
unknown <adetutu.francisb@gmail.com>
2024-02-19 20:19:50 -0500

    update

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -5,17 +5,16 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const NavigationBar = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+    <Navbar className="logo" bg="dark" variant="dark" expand="lg" fixed="top">
       <Container>
-        <Navbar.Brand href="/">Your Logo</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#storefront">Storefront</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#contact">Contact Us</Nav.Link>
-            <Nav.Link href="#login">Login</Nav.Link>
+          <Nav className="ml-auto hover">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="./storefront">Storefront</Nav.Link>
+            <Nav.Link href="./about">About</Nav.Link>
+            <Nav.Link href="./contact">Contact Us</Nav.Link>
+            <Nav.Link href="./login">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
