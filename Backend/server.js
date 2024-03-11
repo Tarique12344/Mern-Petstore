5700159611f32e166e6b7df30da6f4ab6079baaa
Tarique12344 <Tariqueelshamy11@gmail.com>
2024-03-11 12:29:09 -0400

    tried to change server n see if login work

--- Backend/server.js
+++ Backend/server.js
@@ -1,40 +1,38 @@
 // server.js
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes');
+const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const petRoutes = require('./routes/petRoutes');
 const crypto = require('crypto');
 
-const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
+const petRoutes = require('./routes/petRoutes');
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
-
-// middleware
+// Middleware
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
-// view engine
+// View engine
 app.set('view engine', 'ejs');
 
-// database connection
+// Database connection
 const mongoURI = process.env.MONGODB_URI;
 mongoose
   .connect(mongoURI)
-  .then((result) => app.listen(process.env.PORT || 5000))
+  .then(() => app.listen(process.env.PORT || 5000))
   .catch((err) => console.log(err));
 
-// routes
+// Routes
 app.get('/', (req, res) => res.render('home'));
-app.use(petRoutes);
-app.use(authRoutes);
+app.use('/pets', petRoutes); // Assuming you have pet-related routes
+app.use('/auth', authRoutes); // Assuming you want to prefix auth routes with '/auth'
 
 // Generate a random secret key with 32 bytes and convert it to a hexadecimal string
 const secretKey = process.env.SECRET_KEY || crypto.randomBytes(32).toString('hex');
