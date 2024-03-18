4d9a2c692abae8c71f5a7aca7db86fb7336dbbb4
Patrick Annan <you@example.compatrickannan@hotmail.com>
2024-03-18 18:27:22 -0400

    pull

---
+++ frontend/src/context/CartProviderfunc.js
@@ -0,0 +1,41 @@
+// CartProvider.js
+import React, { createContext, useContext, useReducer } from 'react';
+
+const CartContext = createContext();
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload],
+      };
+    case 'REMOVE_FROM_CART':
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((item) => item.id !== action.payload.id),
+      };
+    default:
+      return state;
+  }
+};
+
+const CartProviderfunc = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, { cartItems: [] });
+
+  return (
+    <CartContext.Provider value={{ state, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error('useCart must be used within a CartProvider');
+  }
+  return context;
+};
+
+export { CartProviderfunc, useCart };
