7ed552b75fb9ecb89f17bd3c073b438d99c024b5
Tarique12344 <Tariqueelshamy11@gmail.com>
2024-03-18 19:00:03 -0400

    help

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -23,7 +23,7 @@ const NavigationBar = () => {
   const checkAuthenticationStatus = async () => {
     try {
       // Example API endpoint for checking authentication status
-      const response = await fetch('/authController/checkAuthenticationStatus', {
+      const response = await fetch('/authController', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
