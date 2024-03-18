e8c08ec3a0e22887c9dd15319ff7b721d317fcda
Tarique12344 <Tariqueelshamy11@gmail.com>
2024-03-18 18:19:45 -0400

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/login/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/storefront" element={<StoreFront />} />
         <Route path="/petform" element={<PetForm />} />
