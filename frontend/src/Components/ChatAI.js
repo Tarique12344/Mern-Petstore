1cee4782c42936b0fa109747f25f4fc06e27c599
Tarique12344 <Tariqueelshamy11@gmail.com>
2024-03-20 20:21:33 -0400

    help

--- frontend/src/Components/ChatAI.js
+++ frontend/src/Components/ChatAI.js
@@ -25,20 +25,15 @@ const ChatAi = () => {
             body: JSON.stringify({ message: value }),
             headers: { 'Content-Type': 'application/json' }
         };
-    
+
         try {
             const response = await fetch('https://mern-petstore-backend.onrender.com/completions', options);
             const data = await response.json();
-            if (data.choices && data.choices.length > 0) {
-                setMessage(data.choices[0].message);
-            } else {
-                console.error("No choices found in the response data");
-            }
+            setMessage(data.choices[0].message);
         } catch (error) {
             console.error(error);
         }
     };
-    
 
     useEffect(() => {
         if (!currentTitle && value && message) {
